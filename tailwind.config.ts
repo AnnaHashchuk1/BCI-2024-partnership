abd3dd2b816288d206bfb7c1381f8783f991e2f4
Анна Гащук <annahashchuk@Home-MacBook-Pro.local>
2024-03-03 18:42:02 +0200

    Exports commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "bci-purple": "#945DBD",
+        "bci-gray": "#1E1E1E",
+        "bci-white": "#F0EFF0",
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
       },
     },
   },
