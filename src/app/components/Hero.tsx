d79fd5aea0e2d8e0d8bdc7bca7b5b91e6bb9a5e7
Анна Гащук <annahashchuk@Home-MacBook-Pro.local>
2024-03-04 06:49:36 +0200

    css not showing

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <section className="min-h-screen relative w-full flex flex-col" id="top">
+        <div className="absolute h-[396px] top-[381px] flex flex-row justify-center items-center gap-[22px]">
+            <Image
+                src="/SliderPhoto1.png"
+                width={449}
+                height={313}
+                alt=" "
+            />
+            <Image
+                src="/SliderPhoto2.png"
+                width={449}
+                height={313}
+                alt=" "
+            />
+            <Image
+                src="/SliderPhoto3.png"
+                width={449}
+                height={313}
+                alt=" "
+            />
+            <Image
+                src="/SliderPhoto4.png"
+                width={449}
+                height={313}
+                alt=" "
+            />
+        </div>
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden">
-
+    <main className="overflow-x-hidden">
+      <Hero />
     </main>
   );
 }
