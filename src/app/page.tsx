2b46889efbd4c1cc70ca3571ff8c8c03e86c9920
Анна Гащук <annahashchuk@Home-MacBook-Pro.local>
2024-03-10 19:30:30 +0200

    Collaboration block and updated Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
-import Test from "@/components/Test";
+import Statistics from "@/components/Statistics";
+import Collaboration from "@/components/Collaboration";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden">
     <Header/>
     <Hero />
-    <Test />
+    <Statistics />
+    <Collaboration />
     <Footer />
     </main>
   );
