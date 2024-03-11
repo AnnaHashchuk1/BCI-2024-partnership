1764139b60df52e2fe4e4d74e4be0f4eed0f3bb5
gutovskii <volodimir.gutovskiy@gmail.com>
2024-03-11 22:52:24 +0200

    Why Component (it's name)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
 import Test from "@/components/Test";
+import { Why } from "@/components/Why/Why";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden">
-    <Header/>
-    <Hero />
-    <Test />
-    <Footer />
+      <Header />
+      <Hero />
+      <Why />
+      <Test />
+      <Footer />
     </main>
   );
 }
