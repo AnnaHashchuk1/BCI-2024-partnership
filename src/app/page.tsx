cda29822ee11e08f20a735cb7cba4829c3fcb8c6
gutovskii <volodimir.gutovskiy@gmail.com>
2024-03-12 01:42:38 +0200

    Contacts Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About/About";
 import Collaboration from "@/components/Collaboration";
+import Contacts from "@/components/Contacts/Contacts";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
@@ -15,6 +16,7 @@ export default function Home() {
       <Why />
       <Statistics />
       <Collaboration />
+      <Contacts />
       <Footer />
     </main>
   );
