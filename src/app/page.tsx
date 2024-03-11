c6f03574d1e014c0982f48607fdbff71425e1a07
gutovskii <volodimir.gutovskiy@gmail.com>
2024-03-11 23:02:02 +0200

    fix incorrect merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
+import Collaboration from "@/components/Collaboration";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import { Why } from "@/components/Why/Why";
-import Footer from "@/components/Footer";
 import Statistics from "@/components/Statistics";
-import Collaboration from "@/components/Collaboration";
+import { Why } from "@/components/Why/Why";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden">
-      <Header/>
+      <Header />
       <Hero />
       <Why />
       <Statistics />
