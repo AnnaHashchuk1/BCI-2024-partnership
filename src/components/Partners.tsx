5bfd59c8367cbfe84b1a9ccc9877e40032552669
YuliaSinichak <yuliasinichak@gmail.com>
2024-03-12 11:43:53 +0200

    AboutBEST

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -13,10 +13,10 @@ export default function Partners() {
 
   return (
     <div className="mt-[65px]">
-      <h2 className="text-2xl md:text-[42px] lg:text-[64px] w-[100%] md:w-[75%] mx-auto leading-8 md:leading-[70px] text-white text-center font-montserrat mb-6 md:mb-10 font-bold">
+      <h2 className="text-3xl lg:text-6xl w-[100%] md:w-[75%] mx-auto leading-8 md:leading-[70px] text-white text-center font-montserrat mb-6 md:mb-10 font-bold">
         Партнери попередніх проєктів BESTу
       </h2>
-      <div className="flex flex-wrap justify-center items-center gap-10">
+      <div className="flex flex-wrap justify-center items-center gap-10 mx-20">
         {partnersLogos.map((logoSrc, i) => (
           <div key={i}>
             <Image src={logoSrc} alt={`skuf${i}`} height={200} width={200} />
